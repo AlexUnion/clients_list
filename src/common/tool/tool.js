7b7d06ea1432c2deb88af5cce251a8cb97d2d5c1
Al_chemick <alex2015mig@gmail.com>
2021-03-01 21:55:31 +0200

    'take_out_common_tool'

---
+++ src/common/tool/tool.js
@@ -0,0 +1,8 @@
+export const enableScroll = () => {
+    document.body.style.overflow = "auto";
+};
+export const disableScroll = () => {
+    document.body.style.overflow = "hidden";
+};
+
+export const GRAPHQL_URL = 'https://test-task.expane.pro/api/graphql';
